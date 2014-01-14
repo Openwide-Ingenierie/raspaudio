45da9e6a910cc95fc821eb78ef425d648175d2c8
Jérémy Rosen <jeremy.rosen@openwide.fr>
2014-01-14 11:52:47 +0100

    make the access list a user configuration

---
+++ raspaudio_adjust.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# We can't simply source ${BUILDROOT_CONFIG} as it may contains constructs
+# such as:
+#    BR2_DEFCONFIG="$(CONFIG_DIR)/defconfig"
+# which when sourced from a shell script will eventually try to execute
+# a command name 'CONFIG_DIR', which is plain wrong for virtually every
+# systems out there.
+# So, we have to scan that file instead. Sigh... :-(
+BR2_RASPAUDIO_PAACCESS="$( sed -r -e '/^BR2_RASPAUDIO_PAACCESS="(.*)"$/!d;'    \
+                         -e 's//\1/;'                                           \
+                         "${BUILDROOT_CONFIG}"                                  \
+                )"
+
+
+cat >> $TARGET_DIR/etc/pulse/system.pa <<END_PA
+
+
+### Added for local use
+load-module module-zeroconf-publish
+END_PA
+
+if [ -z $BR2_RASPAUDIO_PAACCESS ] ; then
+	echo "load-module module-native-protocol-tcp auth-anonymous" >> $TARGET_DIR/etc/pulse/system.pa
+else
+	echo "load-module module-native-protocol-tcp auth-ip-acl=$BR2_RASPAUDIO_PAACCESS" >> $TARGET_DIR/etc/pulse/system.pa
+fi
+
